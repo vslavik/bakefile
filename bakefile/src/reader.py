0c148fb1fc6f31164ab1f2c2840dcaec47e2282e
Vaclav Slavik <vslavik@gmail.com>
2003-06-21 22:44:21 +0000

    fixed for target IDs containing -

--- bakefile/src/reader.py
+++ bakefile/src/reader.py
@@ -84,7 +84,7 @@ def handleSet(e, target=None, add_dict=None):
                 if (not overwrite) and (name in target.vars):
                     errors.popCtx()
                     return
-                name = '__%s_%s' % (target.id, name)
+                name = '__%s_%s' % (target.id.replace('-','_'), name)
                 mk.setVar(e.props['var'], '$(%s)' % name,
                              eval=0, target=target,
                              add_dict=add_dict)
