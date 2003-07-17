7363ab66515820ccce2373df4c00ff0f789e92dc
Vaclav Slavik <vslavik@gmail.com>
2003-07-17 14:03:15 +0000

    added script wrapper for win32

---
+++ bakefile/src/win32scriptwrapper.c
@@ -0,0 +1,47 @@
+
+/* This is only a simple script-launcher utility that calls Python interpreter
+   on win32 systems, so that running the tools is simpler.
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <Python.h>
+#include <windows.h>
+
+int main(int argc, char** argv)
+{
+    int ret;
+    char filename[2048];
+    FILE *fp;
+    char *lastp = NULL;
+
+    strncpy(filename, argv[0], 2048);
+    if (GetModuleFileName(NULL, (char*)filename, 2048) != 0)
+    {
+        lastp = strrchr(filename, '.');
+    }
+    if (lastp == NULL)
+    {
+        fprintf(stderr, "Incorrect executable name!\n");
+        return 1;
+    }
+
+    strncpy(lastp, ".py", 2048-(2+lastp-filename));
+
+    fp = fopen(filename, "rb");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Cannot open script file '%s'!\n", filename);
+        return 2;
+    }
+
+    Py_Initialize();
+    PySys_SetArgv(argc, argv);
+    ret = PyRun_SimpleFile(fp, filename);
+    Py_Finalize();
+    fclose(fp);
+
+    if (ret != 0)
+        fprintf(stderr, "Error executing the script!\n");
+    return ret;
+}
