c3e8c540f43537080a056b32c320687823651118
Vaclav Slavik <vslavik@gmail.com>
2003-07-18 10:22:42 +0000

    added script for changing version number

---
+++ chversion.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+replace_ver()
+{
+    cp -f $1 $1.work
+    echo "
+,s@$2@$3@g
+w
+q
+" | ed -s $1.work 2>/dev/null
+}
+
+VER=$1
+
+replace_ver bakefile.spec \
+            '\(Version: *\).*' "\1$VER"
+replace_ver bakefile.iss \
+            '\(#define VERSION *"\).*\("\)' "\1$VER\2"
+replace_ver configure.in \
+            '\(AC_INIT(\[bakefile\], \[\)[^]]*\(\],.*\)' "\1$VER\2"
+replace_ver src/bakefile.py \
+            '\(BAKEFILE_VERSION = "\).*\("\)' "\1$VER\2"
+replace_ver rules/version.bkl \
+            '\(>\).*\(<\)' "\1$VER\2"
