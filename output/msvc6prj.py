e75f048abbdd79d8ca0474539e9dfe6af379edb0
Vaclav Slavik <vslavik@gmail.com>
2003-08-09 10:46:54 +0000

    fixed files ordering of run on unix

--- output/msvc6prj.py
+++ output/msvc6prj.py
@@ -51,8 +51,8 @@ def fixFlagsQuoting(text):
 
 def sortByBasename(files):
     def __sort(x1, x2):
-        f1 = os.path.basename(x1)
-        f2 = os.path.basename(x2)
+        f1 = x1.split('\\')[-1]
+        f2 = x2.split('\\')[-1]
         if f1 == f2: return 0
         elif f1 < f2: return -1
         else: return 1
