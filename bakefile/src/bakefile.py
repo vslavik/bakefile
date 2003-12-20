131fd6309e2a92d4c44afc6486cf540f45fcfbdf
Vaclav Slavik <vslavik@gmail.com>
2003-12-20 21:57:45 +0000

    corrected version check

--- bakefile/src/bakefile.py
+++ bakefile/src/bakefile.py
@@ -114,7 +114,7 @@ def run(args):
         dependencies.save(config.deps_file)
 
 if __name__ == '__main__':
-    if sys.version_info[0:2] < (2,2,2):
+    if sys.version_info[0:3] < (2,2,2):
         sys.stderr.write('error: Bakefile requires at least Python 2.2.2\n')
         sys.exit(1)
 
