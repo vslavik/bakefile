3ad6fadc8590003dfcd75cdeed57b20c35715ee9
Vaclav Slavik <vslavik@gmail.com>
2004-01-01 11:15:18 +0000

    set PYTHONHOME to prevent use of system-wide installed Python

--- bakefile/src/win32scriptwrapper.c
+++ bakefile/src/win32scriptwrapper.c
@@ -10,18 +10,38 @@
 #include <string.h>
 #include <stdio.h>
 #include <Python.h>
+#include <malloc.h>
+#include <stdlib.h>
 #include <windows.h>
+
+/* ----------------------------------------------------------------------- */
+/*       main() - sets PYTHONHOME and calls PyRun_SimpleFile on            */
+/*               script with same name but .py extension                   */
+/* ----------------------------------------------------------------------- */
 
 int main(int argc, char** argv)
 {
     int ret;
     char filename[2048];
+    char dirname[2048];
+    char envvar[2048];
     FILE *fp;
     char *lastp = NULL;
 
     strncpy(filename, argv[0], 2048);
     if (GetModuleFileName(NULL, (char*)filename, 2048) != 0)
     {
+        lastp = strrchr(filename, '\\');
+        if (lastp == NULL)
+        {
+            strncpy(dirname, ".", 2048);
+        }
+        else
+        {
+            strncpy(dirname, filename, lastp - filename);
+            dirname[lastp - filename] = 0;
+        }
+        
         lastp = strrchr(filename, '.');
     }
     if (lastp == NULL)
@@ -38,6 +58,12 @@ int main(int argc, char** argv)
         fprintf(stderr, "Cannot open script file '%s'!\n", filename);
         return 2;
     }
+    
+    /* set PYTHONHOME so that system-wide installed copy of Python is
+       never used; we want to use embedded one: */
+    SetEnvironmentVariable("PYTHONHOME", dirname);
+    sprintf(envvar, "PYTHONHOME=%s", dirname);
+    _putenv(envvar);
 
     Py_Initialize();
     argv[0] = (char*)filename;
