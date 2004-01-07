f5a536d0b8d937b09c70fa67428f8e98a0f1cbe2
Vaclav Slavik <vslavik@gmail.com>
2004-01-07 22:29:12 +0000

    put data files into /usr/share/bakefile, Python code into /usr/lib/bakefile

--- bakefile/src/config.py
+++ bakefile/src/config.py
@@ -10,14 +10,23 @@ import os, os.path, sys
 verbose = 0
 
 # Directories where makefiles are looked for:
+#   1. BAKEFILE_PATHS environment variable
+#   2. if the executable is in $(foo)/lib/bakefile, then in
+#      $(foo)/share/bakefile/{rules,output} if it exists, otherwise in
+#      ../{rules,output} relative to executable location
+#
 searchPath = os.getenv('BAKEFILE_PATHS', '').split(os.pathsep)
 if searchPath == ['']: searchPath = []
-searchPath.append(
-             os.path.normpath(os.path.join(
-             os.path.dirname(os.path.realpath(sys.argv[0])), '..', 'rules')))
-searchPath.append(
-             os.path.normpath(os.path.join(
-             os.path.dirname(os.path.realpath(sys.argv[0])), '..', 'output')))
+
+progdir = os.path.dirname(os.path.realpath(sys.argv[0]))
+datadir = os.path.join(progdir, '..', '..', 'share', 'bakefile')
+if ((os.path.normpath(
+        os.path.join(progdir, '..', '..', 'lib', 'bakefile')) != progdir) or
+        not os.path.isdir(datadir)):
+    datadir = os.path.join(progdir, '..')
+searchPath.append(os.path.normpath(os.path.join(datadir, 'rules')))
+searchPath.append(os.path.normpath(os.path.join(datadir, 'output')))
+
 
 # The way target makefiles quote variables:
 variableSyntax = '$(%s)' # FIXME
