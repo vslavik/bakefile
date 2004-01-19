9ad997cc5739a359d6988a52199ce8ecceea26e4
Vaclav Slavik <vslavik@gmail.com>
2004-01-19 22:08:17 +0000

    added GPL and copyright notices

--- bakefile/src/xmlparser.py
+++ bakefile/src/xmlparser.py
@@ -1,8 +1,25 @@
 #
-# XML loading via either libxml2 (preferred) or Python's builtin minidom
-# module (which uses expat)
+#  This file is part of Bakefile (http://bakefile.sourceforge.net)
 #
-# $Id$
+#  Copyright (C) 2003,2004 Vaclav Slavik
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License version 2 as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#  $Id$
+#
+#  XML loading via either libxml2 (preferred) or Python's builtin minidom
+#  module (which uses expat)
 #
 
 class Element:
