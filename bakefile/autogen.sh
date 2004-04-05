d1d6c3e8c67876799fad5c1180e615a4b12b3cde
Vaclav Slavik <vslavik@gmail.com>
2004-04-05 10:01:46 +0000

    update config.* files too

--- bakefile/autogen.sh
+++ bakefile/autogen.sh
@@ -3,7 +3,7 @@
 mkdir -p admin
 aclocal -I admin
 libtoolize --automake -c -f
-automake --foreign -a -c
+automake --foreign -a -c -f
 autoconf
 (cd src ; swig -python bottlenecks.i)
 (cd doc ; ./process.sh)
