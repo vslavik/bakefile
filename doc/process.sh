2377b2a56d9664faa5e4c370a8d24e337a3a1821
Vaclav Slavik <vslavik@gmail.com>
2005-10-13 22:40:18 +0000

    use XInclude instead of abusing entities for inclusion of subfiles

--- doc/process.sh
+++ doc/process.sh
@@ -2,12 +2,12 @@
 
 echo "validating...."
 #./xml2docbook.py manual.xml >manual.docbook
-xmllint --noout --valid manual.docbook
+xmllint --noout --postvalid --xinclude manual.docbook
 
 echo "converting to HTML...."
 mkdir -p html
-(cd html ; xsltproc ../html.xsl ../manual.docbook)
+(cd html ; xsltproc --xinclude ../html.xsl ../manual.docbook)
 
 echo "making manpage..."
 mkdir -p man
-(cd man ; xsltproc ../manpage.xsl ../manual.docbook)
+(cd man ; xsltproc --xinclude ../manpage.xsl ../manual.docbook)
