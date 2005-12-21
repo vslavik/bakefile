5fc2e53a92e583abb55bee1d5264197675993c81
Vaclav Slavik <vslavik@gmail.com>
2005-12-21 22:00:49 +0000

    better error messages (Francesco Montorsi, patch 1366528)

--- src/mk.py
+++ src/mk.py
@@ -238,7 +238,7 @@ def setVar(name, value, eval=1, target=None, add_dict=None, store_in=None,
         try:
             v = evalExpr(value, target=target, add_dict=add_dict)
         except Exception,e:
-            raise errors.Error("failed to set variable: %s" % e)
+            raise errors.Error("failed to set variable '%s' with value '%s':  %s" % (name, value, e))
     else:
         v = value
     if makevar:
