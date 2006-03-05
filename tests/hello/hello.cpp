dcfddb4e91d65c30451b14e201b8b4682355b20d
Vaclav Slavik <vslavik@gmail.com>
2006-03-05 17:36:57 +0000

    produce compilable (if not working) binaries

--- tests/hello/hello.cpp
+++ tests/hello/hello.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#if defined(BUILD_AS_GUI) && defined(_WINDOWS)
+#include <windows.h>
+int WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+#else
 int main()
+#endif
 {
     std::cout << "Hello, world!" << std::endl;
     return 0;
