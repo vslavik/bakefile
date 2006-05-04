853cad410c30731b6542f97fe4c7163fbb878263
Vaclav Slavik <vslavik@gmail.com>
2006-05-04 10:27:14 +0000

    added <headers> tag (patch #1406524 by Francesco Montorsi)

---
+++ tests/headers/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// this header file is used just to test
+// the header (un)install bakefile support
+#include "hello.h"
+
+int main()
+{
+    func("Hello, world!\n");
+    return 0;
+}
---
+++ tests/headers/hello.h
@@ -0,0 +1,3 @@
+// we use an header file in a subfolder to test if bakefile's <headers> tag
+// (see headers.bkl) correctly handles headers prefixed with a path
+#include "test/hello.h"
