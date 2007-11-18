ec6100ac4156ad5e7eb05219897442b052572c1f
Vaclav Slavik <vslavik@gmail.com>
2007-11-18 17:25:50 +0000

    include Python traceback and exception in reported errors when running with --debug switch (useful when debugging Bakefile itself)

--- src/errors.py
+++ src/errors.py
@@ -53,7 +53,8 @@ def getCtxLocationStr(context=None):
 
 class ErrorBase(Exception):
     def __init__(self, desc, context=None):
-        if context == None: context = _readerContext
+        if context == None:
+            context = _readerContext
         self.desc = desc
         self.context = copy.deepcopy(context)
     def getErrorMessage(self):
@@ -64,8 +65,30 @@ class ErrorBase(Exception):
 class Error(ErrorBase):
     def __init__(self, desc, context=None):
         ErrorBase.__init__(self, desc, context)
+
+        # include additional debugging information if debugging bakefile itself:
+        from config import debug
+        if debug:
+            import traceback
+            self.debuginfo = "\nBacktrace:\n"
+            for i in traceback.format_stack()[-2::-1]:
+                self.debuginfo += i
+            try:
+                exc = traceback.format_exc()
+                if exc != None:
+                    self.debuginfo += "\nException:"
+                    for i in exc.split('\n'):
+                        self.debuginfo += "\n  " + i
+            except ImportError:
+                pass # it's only available in Python 2.4+
+        else:
+            self.debuginfo = None
+
     def __str__(self):
-        return 'error: %s\n%s' % (self.desc, ErrorBase.__str__(self))
+        r = 'error: %s\n%s' % (self.desc, ErrorBase.__str__(self))
+        if self.debuginfo != None:
+            r += self.debuginfo
+        return r
 
 class ReaderError(ErrorBase):
     def __init__(self, el, desc, context=None):
