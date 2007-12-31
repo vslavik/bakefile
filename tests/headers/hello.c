b0c8b495574b1f8b66a899232c7ca9c85c2e14cd
Vaclav Slavik <vslavik@gmail.com>
2007-12-31 09:12:24 +0000

    compilation fixes for the headers test (see bug #91, patch by Francesco Montorsi)

--- tests/headers/hello.c
+++ tests/headers/hello.c
@@ -9,3 +9,26 @@ int main()
     func("Hello, world!\n");
     return 0;
 }
+
+
+// we need to export at least a function to have
+// some toolchains like MSVC's nmake generate
+// the import library (without which the 'install' 
+// target will fail!)
+#ifdef MAKING_THE_DLL
+
+#    if defined(WIN32) && (defined(_MSC_VER) || defined(__BORLANDC__) || defined(__GNUC__) || defined(__WATCOMC__))
+#        define EXPORT __declspec(dllexport)
+#        define IMPORT __declspec(dllimport)
+#    else /* compiler doesn't support __declspec() */
+#        define EXPORT
+#        define IMPORT
+#        error The import library won't be created
+#    endif
+
+void EXPORT doReallyNothing()
+{
+   printf("Have fun with this function!\n");
+}
+
+#endif
