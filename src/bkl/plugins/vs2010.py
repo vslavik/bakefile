d66ca387b0eff6836e3d7a9198c3e6425f30a719
Vaclav Slavik <vslavik@gmail.com>
2011-09-17 17:06:55 +0200

    Create VS2010 filters file.

--- src/bkl/plugins/vs2010.py
+++ src/bkl/plugins/vs2010.py
@@ -292,3 +292,31 @@ class VS2010Toolset(Toolset):
         f.write(XmlFormatter(paths_info).format(root))
         f.commit()
         sln.add(target.name, guid, os.path.basename(filename))
+
+        self._write_filters_file_for(filename)
+
+
+    def _write_filters_file_for(self, filename):
+        f = OutputFile(filename + ".filters", EOL_WINDOWS)
+        f.write("""\
+<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <ItemGroup>
+    <Filter Include="Source Files">
+      <UniqueIdentifier>{4FC737F1-C7A5-4376-A066-2A32D752A2FF}</UniqueIdentifier>
+      <Extensions>cpp;c;cc;cxx;def;odl;idl;hpj;bat;asm;asmx</Extensions>
+    </Filter>
+    <Filter Include="Header Files">
+      <UniqueIdentifier>{93995380-89BD-4b04-88EB-625FBE52EBFB}</UniqueIdentifier>
+      <Extensions>h;hpp;hxx;hm;inl;inc;xsd</Extensions>
+    </Filter>
+    <Filter Include="Resource Files">
+      <UniqueIdentifier>{67DA6AB6-F800-4c08-8B7A-83BB121AAD01}</UniqueIdentifier>
+      <Extensions>rc;ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe;resx;tiff;tif;png;wav;mfcribbon-ms</Extensions>
+    </Filter>
+  </ItemGroup>
+</Project>
+""")
+        f.commit()
+
+
