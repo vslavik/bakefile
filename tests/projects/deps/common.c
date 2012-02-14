a0811b8abe0f42a6fd300def3f700309646faa43
Václav Slavík <vslavik@gmail.com>
2012-02-14 17:07:42 +0100

    Test 'libs' in static libs in tests/projects/deps.

--- tests/projects/deps/common.c
+++ tests/projects/deps/common.c
@@ -1,3 +1,26 @@
+
+#ifdef _WIN32
+  #include <windows.h>
+  #include <wininet.h>
+#else
+  #include <iconv.h>
+#endif
+
+static char buf[2048];
+
+const char *get_os_name()
+{
+#ifdef _WIN32
+    // this is just to force dependency on some external library
+    int i = (int)&InternetOpenUrl;
+    return i % 2 ? "Windows" : "Microsoft Windows";
+#else
+    // this is just to force dependency on some external library
+    int i = (int)&iconv_open;
+    return i % 2 ? "some Unix" : "some UNIX";
+#endif
+}
+
 const char* get_version_info()
 {
     return "1.0";
--- tests/projects/deps/libA.c
+++ tests/projects/deps/libA.c
@@ -2,8 +2,9 @@
 
 // from libcommon:
 extern const char* get_version_info();
+extern const char* get_os_name();
 
 void print_A_banner()
 {
-    printf("library A, version %s\n", get_version_info());
+    printf("library A, version %s running on %s\n", get_version_info(), get_os_name());
 }
