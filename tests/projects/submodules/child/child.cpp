0e27d88746af06767c8ca8c2a64d88e6d50c8eb8
Václav Slavík <vslavik@gmail.com>
2012-02-16 13:51:28 +0100

    Test cross-module dependencies in the "submodules" test.

---
+++ tests/projects/submodules/child/child.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+  puts("Hello from child!");
+  return 0;
+}
---
+++ tests/projects/submodules/main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+  puts("Hello from main project!");
+  return 0;
+}
