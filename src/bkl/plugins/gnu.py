vslavik/bakefile

commit 47bc55b81a0aed2e4f68e688898e52aaca135026

gnu-osx: Fix .d files creation with multiple -arch options.
Apple's GCC doesn't handle the standard -MD -MP flags (which are used to
generate .d files with object files' dependencies on sources and
headers) in presence of multiple -arch options. Clang can handle it, but
we must support GCC. So we run GCC's preprocessor once more to generate
the dependencies, but let's not do it unless necessary, because it a)
costs some time and b) may omit some deps.

src/bkl/plugins/gnu.py
@@ -40,6 +40,33 @@ OSX_ARCH_FLAGS = {
     'x86_64' : '-arch x86_64',
 }
 
+def _is_multiarch_target(target):
+    """
+    Checks if the target builds for >1 archs, i.e. would use more -arch options
+    and be incompatible with gcc's -M* flags.
+    """
+    return len(target["archs"].as_py()) > 1
+
+
+# Apple's GCC doesn't handle the standard -MD -MP flags (which are used to
+# generate .d files with object files' dependencies on sources and headers) in presence
+# of multiple -arch options. Clang can handle it, but we must support GCC. So we run
+# GCC's preprocessor once more to generate the dependencies, but let's not do
+# it unless necessary, because it a) costs some time and b) may omit some deps.
+GCC_DEPS_FLAGS = "-MD -MP"
+OSX_GCC_DEPS_RULES = """
+# Support for generating .d files with multiple -arch options:
+cc_is_clang := $(if $(shell $(CC) --version | grep clang),yes,no)
+ifeq "$(cc_is_clang)" "yes"
+  cc_deps_flags = -MD -MP
+  cc_deps_cmd   =
+else
+  cc_deps_flags =
+  cc_deps_cmd   = $1 -M -MP -o $(patsubst %.o,%.d,$@) $2
+endif
+"""
+
+
 class GnuObjectFileType(FileType):
     name = "gnu-object"
     def __init__(self):
@@ -76,9 +103,14 @@ class GnuCCompiler(GnuFileCompiler):
     _options_prop_name = "c-compiler-options"
 
     def commands(self, toolset, target, input, output):
+        needs_extra_deps_code = (isinstance(toolset, OSXGnuToolset) and
+                                 _is_multiarch_target(target)) # see GCC_DEPS_FLAGS
         cmd = [LiteralExpr("%s -c -o $@ $(CPPFLAGS) $(%s)" %
                 (self._compiler, self._flags_var_name))]
-        cmd += [LiteralExpr("-MD -MP")]
+        if needs_extra_deps_code:
+            cmd += [LiteralExpr("$(cc_deps_flags)")]
+        else:
+            cmd += [LiteralExpr(GCC_DEPS_FLAGS)]
         # FIXME: evaluating the flags here every time is inefficient
         cmd += self._arch_flags(toolset, target)
         cmd += bkl.expr.add_prefix("-D", target["defines"]).items
@@ -88,7 +120,21 @@ class GnuCCompiler(GnuFileCompiler):
         # FIXME: use a parser instead of constructing the expression manually
         #        in here
         cmd.append(input)
-        return [ListExpr(cmd)]
+        retval = [ListExpr(cmd)]
+
+        if needs_extra_deps_code:
+            # add command for generating the deps:
+            cmd = [LiteralExpr("$(call cc_deps_cmd,%s,$(CPPFLAGS) $(%s)" %
+                    (self._compiler, self._flags_var_name))]
+            cmd += bkl.expr.add_prefix("-D", target["defines"]).items
+            cmd += bkl.expr.add_prefix("-I", target["includedirs"]).items
+            cmd += target["compiler-options"].items
+            cmd += target[self._options_prop_name].items
+            cmd.append(input)
+            cmd.append(LiteralExpr(")"))
+            retval.append(ListExpr(cmd))
+
+        return retval
 
 
 class GnuCXXompiler(GnuCCompiler):
@@ -196,7 +242,7 @@ class GnuToolset(MakefileToolset):
     dll_prefix = "lib"
     dll_extension = "so"
 
-    def on_header(self, file):
+    def on_header(self, file, module):
         file.write("""
 # This file was automatically generated by bakefile.
 #
@@ -222,7 +268,7 @@ RANLIB ?= ranlib
     def on_phony_targets(self, file, targets):
         file.write(".PHONY: %s\n" % " ".join(targets))
 
-    def on_footer(self, file):
+    def on_footer(self, file, module):
         file.write("\n"
                    "# Dependencies tracking:\n"
                    "-include *.d\n")
@@ -243,3 +289,8 @@ class OSXGnuToolset(GnuToolset):
     default_makefile = "Makefile.osx"
 
     dll_extension = "dylib"
+
+    def on_footer(self, file, module):
+        if list(t for t in module.targets.itervalues() if _is_multiarch_target(t)):
+            file.write(OSX_GCC_DEPS_RULES)
+        super(OSXGnuToolset, self).on_footer(file, module)

src/bkl/makefile.py
@@ -221,7 +221,7 @@ class MakefileToolset(Toolset):
         expr_fmt = _MakefileExprFormatter(self.Formatter, paths_info)
 
         f = io.OutputFile(output, io.EOL_UNIX)
-        self.on_header(f)
+        self.on_header(f, module)
 
         for v in module.variables:
             pass
@@ -310,7 +310,7 @@ class MakefileToolset(Toolset):
         f.write(self.Formatter.target(name="clean", deps=[], commands=clean_cmds))
 
         self.on_phony_targets(f, phony_targets)
-        self.on_footer(f)
+        self.on_footer(f, module)
 
         f.commit()
 
@@ -325,7 +325,7 @@ class MakefileToolset(Toolset):
         for subname, subdir, subfile, subdeps in submakefiles:
             yield self.Formatter.submake_command(subdir, subfile, "clean")
 
-    def on_header(self, file):
+    def on_header(self, file, module):
         """
         Called before starting generating the output to add any header text,
         typically used to insert a warning about the file being auto-generated.
@@ -339,7 +339,7 @@ class MakefileToolset(Toolset):
         """
         pass
 
-    def on_footer(self, file):
+    def on_footer(self, file, module):
         """
         Called at the end of generating the output to add any ending text, for
         example unconditional inclusion of dependencies tracking code.

commit 20d8fde478142dff4c80c5426e75e223e90a3edf

Don't fail if "arch" property is not available.
Fixes exception introduced by 47bc55b81a0aed2e4f68e688898e52aaca135026.

Also don't check all targets, stop on the first one.

src/bkl/plugins/gnu.py
@@ -45,7 +45,10 @@ def _is_multiarch_target(target):
     Checks if the target builds for >1 archs, i.e. would use more -arch options
     and be incompatible with gcc's -M* flags.
     """
-    return len(target["archs"].as_py()) > 1
+    try:
+        return len(target["archs"].as_py()) > 1
+    except KeyError:
+        return False # not an executable
 
 
 # Apple's GCC doesn't handle the standard -MD -MP flags (which are used to
@@ -291,6 +294,8 @@ class OSXGnuToolset(GnuToolset):
     dll_extension = "dylib"
 
     def on_footer(self, file, module):
-        if list(t for t in module.targets.itervalues() if _is_multiarch_target(t)):
-            file.write(OSX_GCC_DEPS_RULES)
+        for t in module.targets.itervalues():
+            if _is_multiarch_target(t):
+                file.write(OSX_GCC_DEPS_RULES)
+                break
         super(OSXGnuToolset, self).on_footer(file, module)