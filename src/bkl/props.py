6db5092f7400b8bd62f90fc6f3eb397a431f1d64
Václav Slavík <vslavik@gmail.com>
2012-04-08 15:31:22 +0200

    Sort allowed toolset names list.

--- src/bkl/props.py
+++ src/bkl/props.py
@@ -117,7 +117,7 @@ def std_target_props():
 
 def std_module_props():
     """Creates list of all standard module properties."""
-    toolsets_enum_type = EnumType("toolset", api.Toolset.all_names())
+    toolsets_enum_type = EnumType("toolset", sorted(api.Toolset.all_names()))
 
     return [
         Property("toolsets",
@@ -130,7 +130,7 @@ def std_module_props():
 
 def std_project_props():
     """Creates list of all standard project properties."""
-    toolsets_enum_type = EnumType("toolset", api.Toolset.all_names())
+    toolsets_enum_type = EnumType("toolset", sorted(api.Toolset.all_names()))
 
     return [
         Property("toolset",
