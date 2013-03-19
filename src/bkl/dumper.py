vslavik/bakefile

commit 1cf7c0cd0fddd1728eb9c86844aae2227da6a281

Fix GUIDs of VS solution folders to be unique.
Submodules with the same name (e.g. 'tests' under several different
components) would incorrectly generate their GUID for .sln file from
just the short, non-unique name. This was incorrect and resulted in
confusing VS with what it regarded as duplicate solution folders.

src/bkl/model.py
@@ -652,8 +652,6 @@ class Module(ModelPart):
     def __str__(self):
         return "module %s" % self.source_file
 
-    name = property(lambda self: self.source_file)
-
     def child_parts(self):
         return self.targets.itervalues()
 

src/bkl/plugins/vsbase.py
@@ -356,7 +356,8 @@ class VSSolutionBase(object):
     def __init__(self, toolset, module):
         slnfile = module["%s.solutionfile" % toolset.name].as_native_path_for_output(module)
         self.name = module.name
-        self.guid = GUID(NAMESPACE_SLN_GROUP, module.project.top_module.name, module.name)
+        # unlike targets, modules' names aren't globally unique, so use the source file, which is
+        self.guid = GUID(NAMESPACE_SLN_GROUP, module.project.top_module.name, module.source_file)
         self.projects = OrderedDict()
         self.subsolutions = []
         self.parent_solution = None

commit caef4a4df2948aa969221193d624d5ac9ef304aa

Introduce Module.fully_qualified_name, use it to compute SLN GUIDs.
Fixes regression introduced in 1cf7c0c, where SLN files with groups in
them would be different when generated on Unix and on Windows.

src/bkl/dumper.py
@@ -53,8 +53,7 @@ def dump_module(module):
     """
     if len(module.project.modules) > 1:
         # use Unix filename syntax in the dumps even on Windows
-        filename = module.source_file.replace("\\", "/")
-        out = "module %s {\n" % filename
+        out = "module %s {\n" % module.fully_qualified_name
     else:
         out = "module {\n"
 

src/bkl/model.py
@@ -248,6 +248,14 @@ class ModelPart(object):
             prj = prj.parent
         return prj
 
+    @memoized_property
+    def fully_qualified_name(self):
+        """
+        Fully qualified name of this part. E.g. "main::submodule::mylibrary".
+        """
+        s = self.parent.fully_qualified_name
+        return "%s::%s" % (s, self.name) if s else self.name
+
 
     def child_parts(self):
         """
@@ -496,6 +504,7 @@ class Project(ModelPart):
 
     def __init__(self):
         super(Project, self).__init__(parent=None)
+        self.fully_qualified_name = ""
         self.modules = []
         self.configurations = utils.OrderedDict()
         self.settings = utils.OrderedDict()
@@ -670,7 +679,7 @@ class Module(ModelPart):
 
     @memoized_property
     def name(self):
-        """Name of the module"""
+        """Name of the module. Note that this is not globally unique."""
         return os.path.splitext(os.path.basename(self.source_file))[0]
 
     @property

src/bkl/plugins/vsbase.py
@@ -356,8 +356,8 @@ class VSSolutionBase(object):
     def __init__(self, toolset, module):
         slnfile = module["%s.solutionfile" % toolset.name].as_native_path_for_output(module)
         self.name = module.name
-        # unlike targets, modules' names aren't globally unique, so use the source file, which is
-        self.guid = GUID(NAMESPACE_SLN_GROUP, module.project.top_module.name, module.source_file)
+        # unlike targets, modules' names aren't globally unique, so use the fully qualified name, which is
+        self.guid = GUID(NAMESPACE_SLN_GROUP, module.project.top_module.name, module.fully_qualified_name)
         self.projects = OrderedDict()
         self.subsolutions = []
         self.parent_solution = None

tests/projects/submodules/main.model
@@ -1,4 +1,4 @@
-module submodules/main.bkl {
+module main {
   submodules {
     submodules/lib/libcommon.bkl
     submodules/child/child.bkl
@@ -17,7 +17,7 @@ module submodules/main.bkl {
   }
 }
 
-module submodules/lib/libcommon.bkl {
+module main::libcommon {
   variables {
     vs2008.solutionfile = @top_srcdir/lib/common2008.sln
   }
@@ -30,7 +30,7 @@ module submodules/lib/libcommon.bkl {
   }
 }
 
-module submodules/child/child.bkl {
+module main::child {
   variables {
     vs2008.solutionfile = @top_srcdir/child/child2008.sln
   }