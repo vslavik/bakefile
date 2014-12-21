vslavik/bakefile

commit 58ffebfe95b3e64b1b43c639cf8fda4302d25e16

Simplify paths under the build directory in gnu toolset output.
Instead of using @top_srcdir/$(builddir)/$(subdir) everywhere, which resulted
in complex paths when the bakefile itself wasn't in the top_srcdir, use a
helper _builddir make variable defined only once.

In addition to making the generated makefiles shorter and simpler, this also
allows to use just "make target" with them instead of having to explicitly use
something like "make ../build/target" even when builddir was not set.

Finally, as an extra bonus, defining the helper variable allows us to create
the build directory automatically, while it had to be done manually before
building before.

src/bkl/makefile.py
@@ -164,14 +164,8 @@ class MakefileToolset(Toolset):
                        inheritable=False,
                        doc="Name of output file for module's makefile.")
 
-    def get_module_builddir(self, module):
-        makefile = module["%s.makefile" % self.name]
-        builddir = makefile.get_directory_path()
-        builddir.components.insert(0, expr.LiteralExpr("$(builddir)"))
-        return builddir
-
     def get_builddir_for(self, target):
-        return self.get_module_builddir(target.parent)
+        return expr.PathExpr([], expr.ANCHOR_BUILDDIR)
 
     def generate(self, project):
         # We need to know build graphs of all targets so that we can generate
@@ -205,7 +199,7 @@ class MakefileToolset(Toolset):
         paths_info = expr.PathAnchorsInfo(
                 dirsep="/", # FIXME - format-configurable
                 outfile=output,
-                builddir=self.get_module_builddir(module).as_native_path_for_output(module),
+                builddir=None,
                 model=module)
 
         mk_fmt = self.Formatter()
@@ -213,11 +207,6 @@ class MakefileToolset(Toolset):
 
         f = io.OutputFile(output, io.EOL_UNIX, creator=self, create_for=module)
         self.on_header(f, module)
-        f.write("""
-# The directory for the build files, may be overridden on make command line.
-builddir = .
-
-""")
 
         self._gen_settings(module, mk_fmt, expr_fmt, f)
 

src/bkl/plugins/gnu.py
@@ -303,6 +303,18 @@ class GnuExprFormatter(MakefileExprFormatter):
         MakefileExprFormatter.__init__(self, paths_info)
         self.toolset = toolset
 
+    def path(self, e):
+        # We handle all build paths in a very special way to allow customizing
+        # them at make time by setting the make builddir variable, which is
+        # used to initialize another make variable called _builddir which is
+        # then used to construct all build paths.
+        if e.anchor == bkl.expr.ANCHOR_BUILDDIR:
+            # Notice that _builddir is either empty or contains the
+            # trailing slash, so we must not add another one here.
+            return "$(_builddir)" + "/".join(self.format(c) for c in e.components)
+
+        return super(GnuExprFormatter, self).path(e)
+
     def bool_value(self, e):
         self.toolset.uses_non_std_bool_macros = True
         return "$(_true)" if e.value else "$(_false)"
@@ -487,6 +499,32 @@ CXX := %s
         # macros, if they turn out to be really needed, or nothing otherwise.
         file.write(GMAKE_IFEXPR_MACROS_PLACEHOLDER)
         self.uses_non_std_bool_macros = False
+
+        # Build the value actually representing the build directory, it is
+        # only used here (see GnuExprFormatter.path) and only to initialize
+        # the internal _builddir in the fragment below.
+        makefile = module["%s.makefile" % self.name]
+        module_builddir = makefile.get_directory_path()
+        module_builddir.components.insert(0, bkl.expr.LiteralExpr("$(builddir)"))
+
+        paths_info = bkl.expr.PathAnchorsInfo(
+                dirsep="/",
+                outfile=makefile.as_native_path_for_output(module),
+                builddir=None,
+                model=module)
+
+        builddir_path = GnuExprFormatter(self, paths_info).visit(module_builddir)
+
+        file.write("""
+# The directory for the build files, may be overridden on make command line.
+builddir = .
+
+ifneq ($(builddir),.)
+_builddir := $(shell mkdir -p %s && echo $$_/)
+endif
+
+""" % builddir_path)
+
 
     def on_phony_targets(self, file, targets):
         file.write(".PHONY: %s\n" % " ".join(targets))

src/bkl/interpreter/passes.py
@@ -200,7 +200,6 @@ class PathsNormalizer(RewritingVisitor):
     @memoized
     def _builddir(self, target):
         builddir = self.toolset.get_builddir_for(target)
-        assert builddir.anchor != bkl.expr.ANCHOR_BUILDDIR
         logger.debug('translating @builddir paths of %s into %s', target, builddir)
         return builddir
 

commit 1e05852e7a7e4ec1d8982bef0b647e4161fe3500

Fix paths for the dependencies in the generated makefiles.
The changes of 58ffebfe95b3e64b1b43c639cf8fda4302d25e16 broke the paths for
the dependencies in different directories as they simplified a bit too much.
Restore the non $(builddir)-related part of MakefileToolset.get_builddir_for
to correct this and fix the discrepancy between targets referenced outside of
their own makefile and those only referenced in it by using special new
ANCHOR_TOP_BUIDLDIR for the former.

src/bkl/expr.py
@@ -441,9 +441,10 @@ class IfExpr(Expr):
 ANCHOR_SRCDIR = "@srcdir"
 ANCHOR_TOP_SRCDIR = "@top_srcdir"
 ANCHOR_BUILDDIR = "@builddir"
+ANCHOR_TOP_BUILDDIR = "@top_builddir"
 
 # all possible anchors
-ANCHORS = [ANCHOR_SRCDIR, ANCHOR_TOP_SRCDIR, ANCHOR_BUILDDIR]
+ANCHORS = [ANCHOR_SRCDIR, ANCHOR_TOP_SRCDIR, ANCHOR_BUILDDIR, ANCHOR_TOP_BUILDDIR]
 
 class PathExpr(Expr):
     """
@@ -467,8 +468,12 @@ class PathExpr(Expr):
          overridden in it).
 
        * ``expr.ANCHOR_BUILDDIR`` -- Path is relative to the build directory.
-         This anchor should be used for all transient files (object files
-         or other generated files).
+         Either this anchor or ``expr.ANCHOR_TOP_BUILDDIR`` should be used for
+         all transient files (object files or other generated files).
+
+       * ``expr.ANCHOR_TOP_BUILDDIR`` -- Path is relative to the top build
+         directory. This is currently only used by makefile backends where the
+         build directory for a sub-makefile is different from the top one.
 
     .. attribute:: anchor_file
 
@@ -606,7 +611,7 @@ class PathExpr(Expr):
 
         TODO: add a @absdir anchor instead?
         """
-        if self.anchor == ANCHOR_BUILDDIR or not self.components:
+        if self.anchor in [ANCHOR_BUILDDIR, ANCHOR_TOP_BUILDDIR] or not self.components:
             return False
         first = self.components[0]
         return isinstance(first, PlaceholderExpr)

src/bkl/makefile.py
@@ -165,7 +165,9 @@ class MakefileToolset(Toolset):
                        doc="Name of output file for module's makefile.")
 
     def get_builddir_for(self, target):
-        return expr.PathExpr([], expr.ANCHOR_BUILDDIR)
+        makefile = target["%s.makefile" % self.name]
+        builddir = makefile.get_directory_path()
+        return expr.PathExpr(builddir.components, expr.ANCHOR_TOP_BUILDDIR)
 
     def generate(self, project):
         # We need to know build graphs of all targets so that we can generate

src/bkl/plugins/gnu.py
@@ -26,6 +26,7 @@
 GNU tools (GCC, GNU Make, ...) toolset.
 """
 
+import os.path
 from bkl.api import FileCompiler, FileType
 from bkl.makefile import MakefileToolset, MakefileFormatter, MakefileExprFormatter
 import bkl.compilers
@@ -315,7 +316,19 @@ class GnuExprFormatter(MakefileExprFormatter):
             self.toolset.uses_builddir = True
             return "$(_builddir)" + "/".join(self.format(c) for c in e.components)
 
-        return super(GnuExprFormatter, self).path(e)
+        super_self = super(GnuExprFormatter, self)
+
+        if e.anchor == bkl.expr.ANCHOR_TOP_BUILDDIR:
+            self.toolset.uses_builddir = True
+
+            # To handle top build directory-relative paths correctly, just
+            # interpret the path relatively to the top source directory.
+            p = super_self.path(bkl.expr.PathExpr(e.components, bkl.expr.ANCHOR_TOP_SRCDIR))
+
+            # But then root it at build directory.
+            return "$(_builddir)" + p
+
+        return super_self.path(e)
 
     def bool_value(self, e):
         self.toolset.uses_non_std_bool_macros = True