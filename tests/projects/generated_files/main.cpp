165267069014024950b0fcb7f386ac0d140a0c84
Vadim Zeitlin <vadim@zeitlins.org>
2014-12-24 16:45:13 +0100

    Extend generated files test to show a command with multiple outputs.
    This is actually more realistic than the same command producing two different
    outputs and is also handled a bit differently, so is worth testing for.

--- tests/projects/generated_files/main.cpp
+++ tests/projects/generated_files/main.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "gensrc.h"
+#include "gensrc2.h"
 
 int main()
 {
-    printf("get_msg returned \"%s\"\n", get_msg());
+    printf("first generated file contents is \"%s\"\n", get_gensrc_body());
+    printf("second generated file contents is \"%s\"\n", get_gensrc2_body());
     return 0;
 }
