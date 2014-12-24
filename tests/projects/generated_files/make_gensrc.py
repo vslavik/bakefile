9d3d50ecfc5f1be6cdba13e89d2e56444e992784
Vadim Zeitlin <vadim@zeitlins.org>
2014-12-24 16:45:13 +0100

    Extend generated files test to show a command with multiple outputs.
    This is actually more realistic than the same command producing two different
    outputs and is also handled a bit differently, so is worth testing for.

--- tests/projects/generated_files/make_gensrc.py
+++ tests/projects/generated_files/make_gensrc.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python
 
 import sys
+import os.path
 
 if len(sys.argv) != 4:
-    raise RuntimeError("invalid arguments")
+    raise RuntimeError("invalid arguments, usage: %s --<both|header|source> output input" % sys.argv[0])
 
-with file(sys.argv[2], "wt") as f:
-    if sys.argv[1] == "--header":
-        f.write("extern const char *get_msg();\n")
-    elif sys.argv[1] == "--source":
-        f.write("const char *get_msg() { return \"Hello!\"; }\n")
-    else:
-        raise RuntimeError("invalid argument %s" % sys.argv[1])
+def gen_header(outname):
+    dirname, basename = os.path.split(outname)
+    name, ext = os.path.splitext(basename)
+    with file(os.path.join(dirname, name + ".h"), "wt") as outf:
+        outf.write("extern const char *get_%s_body();\n" % name)
+
+def gen_source(outname, msg):
+    dirname, basename = os.path.split(outname)
+    name, ext = os.path.splitext(basename)
+    with file(os.path.join(dirname, name + ".cpp"), "wt") as outf:
+        with file(msg, "rt") as inpf:
+            outf.write("const char *get_%s_body() { return \"" % name + inpf.read().rstrip('\n') + "\"; }\n")
+
+if sys.argv[1] == "--both":
+    gen_header(sys.argv[2])
+    gen_source(sys.argv[2], sys.argv[3])
+elif sys.argv[1] == "--header":
+    gen_header(sys.argv[2])
+elif sys.argv[1] == "--source":
+    gen_source(sys.argv[2], sys.argv[3])
+else:
+    raise RuntimeError("invalid argument %s" % sys.argv[1])
