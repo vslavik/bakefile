fa088c68181b70571af86dc012ef5856fa3f04a7
Vadim Zeitlin <vadim@zeitlins.org>
2015-11-29 02:29:16 +0100

    Don't collapse "ImportGroup" tag in VS 201x output
    This removes a gratuitous difference with the project files generated by the
    IDE.
    
    Add VS201xXmlFormatter and set it up as XmlFormatter for VS201xToolsetBase.

--- src/bkl/plugins/vs201x.py
+++ src/bkl/plugins/vs201x.py
@@ -32,6 +32,16 @@ from bkl.plugins.vsbase import *
 from bkl.expr import concat, format_string
 
 
+class VS201xXmlFormatter(XmlFormatter):
+    """
+    XmlFormatter for VS 201x output.
+    """
+
+    elems_not_collapsed = set(["ImportGroup"])
+
+    def __init__(self, settings, paths_info):
+        super(VS201xXmlFormatter, self).__init__(settings, paths_info)
+
 # TODO: Put more content into this class, use it properly
 class VS2010Project(VSProjectBase):
     """
@@ -51,6 +61,9 @@ class VS2010Project(VSProjectBase):
 
 class VS201xToolsetBase(VSToolsetBase):
     """Base class for VS2010, VS2012, VS2013 and VS2015 toolsets."""
+
+    #: XML formatting class
+    XmlFormatter = VS201xXmlFormatter
 
     #: Extension of format files
     proj_extension = "vcxproj"
@@ -348,7 +361,7 @@ class VS201xToolsetBase(VSToolsetBase):
         filename = project.projectfile.as_native_path_for_output(target)
         paths_info = self.get_project_paths_info(target, project)
 
-        formatter = XmlFormatter(target.project.settings, paths_info)
+        formatter = self.XmlFormatter(target.project.settings, paths_info)
         f = OutputFile(filename, EOL_WINDOWS,
                        creator=self, create_for=target)
         f.write(codecs.BOM_UTF8)
