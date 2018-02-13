be1476e39753f74a90a255c76711d80db7bf6cf0
Václav Slavík <vaclav@slavik.io>
2018-02-13 20:29:48 +0100

    Require Python 2.7

--- src/bkl/version.py
+++ src/bkl/version.py
@@ -1,7 +1,7 @@
 #
 #  This file is part of Bakefile (http://bakefile.org)
 #
-#  Copyright (C) 2012-2013 Vaclav Slavik
+#  Copyright (C) 2012-2018 Vaclav Slavik
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to
@@ -35,29 +35,24 @@ def get_version():
     if os.path.exists(gitdir):
         import subprocess
         try:
-            # TODO-2.6: replace this with subprocess.check_output() once we
-            # don't need to support 2.6 any longer.
-            process = subprocess.Popen(["git", "--git-dir=%s" % gitdir, "describe"], stdout=subprocess.PIPE)
-            ver, unused_stderr = process.communicate()
-            if process.poll() == 0:
-                ver = ver.strip()
-                if ver:
-                    if ver[0] == "v": ver = ver[1:]
-                    version_fields = ver.split("-")
-                    version = version_fields[0]
-                    if len(version_fields) > 1:
-                        # if we're using a 2 component version, transform it
-                        # into 3 component one to ensure that versions are
-                        # compared correctly: we want to have
-                        #
-                        #       1.2 < 1.2-git < 1.2.1
-                        #
-                        # so the 2nd git version after 1.2 must be "1.2.0.2"
-                        # and not just "1.2.2" which wouldn't compare correctly
-                        if len(version.split(".")) < 3: version += ".0"
-                        return "%s-%s" % (version, version_fields[1])
-                    else:
-                        return version
+            ver = subprocess.check_output(["git", "--git-dir=%s" % gitdir, "describe"]).strip()
+            if ver:
+                if ver[0] == "v": ver = ver[1:]
+                version_fields = ver.split("-")
+                version = version_fields[0]
+                if len(version_fields) > 1:
+                    # if we're using a 2 component version, transform it
+                    # into 3 component one to ensure that versions are
+                    # compared correctly: we want to have
+                    #
+                    #       1.2 < 1.2-git < 1.2.1
+                    #
+                    # so the 2nd git version after 1.2 must be "1.2.0.2"
+                    # and not just "1.2.2" which wouldn't compare correctly
+                    if len(version.split(".")) < 3: version += ".0"
+                    return "%s-%s" % (version, version_fields[1])
+                else:
+                    return version
         # fall back to normal version information in case of any error
         # (e.g. missing git or problem running it):
         except Exception:
