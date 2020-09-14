vslavik/bakefile

commit 146b5779d0060577c313295eb9ec8820b282e9e9

Allow specifying extra property files for generated MSVS projects
Add "vs.property-sheets" option allowing specifying the extra property
sheets to include in the generated MSVS projects.

Note that VSToolsetBase.properties_target() was renamed to
properties_target_vsbase() and is now explicitly called from
properties_target() implementation in the derived classes.

src/bkl/plugins/vs200x.py
@@ -176,6 +176,10 @@ class VS200xToolsetBase(VSToolsetBase):
     #: Whether Detect64BitPortabilityProblems is supported
     detect_64bit_problems = True
 
+    @classmethod
+    def properties_target(cls):
+        return cls.properties_target_vsbase()
+
     def gen_for_target(self, target, project):
         root = Node("VisualStudioProject")
         root["ProjectType"] = "Visual C++"

src/bkl/plugins/vs201x.py
@@ -30,6 +30,7 @@ from bkl.io import OutputFile, EOL_WINDOWS
 
 from bkl.plugins.vsbase import *
 from bkl.expr import concat, format_string
+from bkl.vartypes import ListType, PathType
 
 
 class VS201xXmlFormatter(XmlFormatter):
@@ -73,6 +74,25 @@ class VS201xToolsetBase(VSToolsetBase):
 
     #: ToolsVersion property
     tools_version = "4.0"
+
+    properties_target_vs201x = [
+            Property("vs.property-sheets",
+                     type=ListType(PathType()),
+                     default=bkl.expr.NullExpr(),
+                     inheritable=False,
+                     doc="""
+                         May contain paths to one or more property sheets files
+                         that will be imported from the generated project if they
+                         exist.
+                         """)
+        ]
+
+    @classmethod
+    def properties_target(cls):
+        for p in cls.properties_target_vsbase():
+            yield p
+        for p in cls.properties_target_vs201x:
+            yield p
 
     def gen_for_target(self, target, project):
         rc_files = []
@@ -144,6 +164,17 @@ class VS201xToolsetBase(VSToolsetBase):
                   Project="$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props",
                   Condition="exists('$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props')",
                   Label="LocalAppDataPlatform")
+
+            for property_sheet in cfg["vs.property-sheets"]:
+                # For now, always add exists() condition unconditionally as
+                # it's often useful to have it, but we don't provide any way
+                # to request it explicitly and not having it would be more
+                # problematic than using it unnecessarily (which is not really
+                # a problem in practice).
+                n.add("Import",
+                      Project=property_sheet,
+                      Condition=concat("exists('", property_sheet, "')"))
+
             root.add(n)
 
         root.add("PropertyGroup", Label="UserMacros")

src/bkl/plugins/vsbase.py
@@ -720,7 +720,7 @@ class VSToolsetBase(Toolset):
     loadable_module_extension = "dll"
 
     @classmethod
-    def properties_target(cls):
+    def properties_target_vsbase(cls):
         yield Property("%s.projectfile" % cls.name,
                        type=PathType(),
                        default=update_wrapper(partial(_project_name_from_solution, cls), _project_name_from_solution),

extras/vim/bkl.vim
@@ -54,6 +54,7 @@ syn keyword	bklCommonProp	vs2012.guid vs2012.projectfile contained
 syn keyword	bklCommonProp	vs2013.guid vs2013.projectfile contained
 syn keyword	bklCommonProp	vs2015.guid vs2015.projectfile contained
 syn keyword	bklCommonProp	vs2017.guid vs2017.projectfile contained
+syn keyword	bklCommonProp	vs.property-sheets contained
 syn match	bklCommonProp	"vs\(2003\|2005\|2008\|2010\|2012\|2013\|2015\|2017\)\.option\(\.\w\+\)\{1,2}" contained
 
 " Properties that can occur inside action targets only.

commit 261e89599ef0ddbfd233f3cf132d5bbda43bcb4d

Fix generating properties documentation for VS toolsets
This was broken in 146b5779 (Allow specifying extra property files for
generated MSVS projects, 2019-02-11), which introduced a couple of
helper methods/variables starting with "properties_" that were picked up
by all_properties_kinds() used in docs/gen_reference.py even though they
were never supposed to.

Fix this by simply renaming these identifiers to not start with the
magic "properties_" prefix.

src/bkl/plugins/vs200x.py
@@ -178,7 +178,7 @@ class VS200xToolsetBase(VSToolsetBase):
 
     @classmethod
     def properties_target(cls):
-        return cls.properties_target_vsbase()
+        return cls.vsbase_target_properties()
 
     def gen_for_target(self, target, project):
         root = Node("VisualStudioProject")

src/bkl/plugins/vs201x.py
@@ -75,7 +75,7 @@ class VS201xToolsetBase(VSToolsetBase):
     #: ToolsVersion property
     tools_version = "4.0"
 
-    properties_target_vs201x = [
+    vs201x_target_properties = [
             Property("vs.property-sheets",
                      type=ListType(PathType()),
                      default=bkl.expr.NullExpr(),
@@ -89,9 +89,9 @@ class VS201xToolsetBase(VSToolsetBase):
 
     @classmethod
     def properties_target(cls):
-        for p in cls.properties_target_vsbase():
+        for p in cls.vsbase_target_properties():
             yield p
-        for p in cls.properties_target_vs201x:
+        for p in cls.vs201x_target_properties:
             yield p
 
     def gen_for_target(self, target, project):

src/bkl/plugins/vsbase.py
@@ -744,7 +744,7 @@ class VSToolsetBase(Toolset):
     loadable_module_extension = "dll"
 
     @classmethod
-    def properties_target_vsbase(cls):
+    def vsbase_target_properties(cls):
         yield Property("%s.projectfile" % cls.name,
                        type=PathType(),
                        default=update_wrapper(partial(_project_name_from_solution, cls), _project_name_from_solution),